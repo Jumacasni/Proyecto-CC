7cb9a8e3940e22c6791cc97d0aaa6cbc91e61de0
Juan Manuel Castillo Nievas <jumacasni@gmail.com>
2020-10-24 15:42:33 +0200

    creacion clase usuario. Avanza HU #10. Closes #24

---
+++ src/usuario.js
@@ -0,0 +1,24 @@
+class Usuario{
+	constructor(usuario, nombre){
+		this.usuario = usuario;
+		this.nombre = nombre;
+		this.ubicacion = "";
+		this.notificaciones_terremotos = true;
+	}
+
+	getUsuario(){
+
+	}
+
+	getNombre(){
+
+	}
+
+	getUbicacion(){
+
+	}
+
+	getNotificacionesTerremotos(){
+
+	}
+}
