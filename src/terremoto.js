2418c13e6740fad2e6971834806e3ccf5fbc8aa1
Juan Manuel Castillo Nievas <jumacasni@gmail.com>
2020-10-24 15:40:23 +0200

    creacion clase terremoto. Avanza HU #9 y #10. Closes #22

---
+++ src/terremoto.js
@@ -0,0 +1,39 @@
+class Terremoto{
+	constructor(fecha, hora, latitud, longitud, profundidad, magnitud, localizacion){
+		this.fecha = fecha;
+		this.hora = hora;
+		this.latitud = latitud;
+		this.longitud = longitud;
+		this.profundidad = profundidad;
+		this.magnitud = magnitud;
+		this.localizacion = localizacion;
+	}
+
+	getFecha(){
+
+	}
+
+	getHora(){
+
+	}
+
+	getLatitud(){
+
+	}
+
+	getLongitud(){
+
+	}
+
+	getProfundidad(){
+
+	}
+
+	getMagnitud(){
+
+	}
+
+	getLocalizacion(){
+		
+	}
+}
