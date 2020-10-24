343c9c95907bc417f74f4969c578f8a1f42cd47d
Juan Manuel Castillo Nievas <jumacasni@gmail.com>
2020-10-24 15:41:34 +0200

    creacion clase terremoto_reportado. Avanza HU #10. Closes #23

---
+++ src/terremoto_reportado.js
@@ -0,0 +1,34 @@
+class TerremotoReportado{
+	constructor(usuario, fecha, hora, latitud, longitud, localizacion){
+		this.usuario = usuario;
+		this.fecha = fecha;
+		this.hora = hora;
+		this.latitud = latitud;
+		this.longitud = longitud;
+		this.localizacion = localizacion;
+	}
+
+	getUsuario(){
+
+	}
+
+	getFecha(){
+
+	}
+
+	getHora(){
+
+	}
+
+	getLatitud(){
+
+	}
+
+	getLongitud(){
+
+	}
+
+	getLocalizacion(){
+		
+	}
+}
