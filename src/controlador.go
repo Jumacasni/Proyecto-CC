2ce244c3093f22a73641bd0c113b8df3b32ba7ef
Juan Manuel Castillo Nievas <jumacasni@gmail.com>
2020-10-31 11:15:43 +0100

    creada clase controlador. #44 y #46

---
+++ src/controlador.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"src/terremoto"
+)
+
+type Controlador struct {  
+	terremotos []terremoto.Terremoto
+}
+
+func NewControlador() *Controlador{
+	// Inicializa terremotos vacíos
+	controlador := Controlador{terremotos: []terremoto.Terremoto{}}
+
+	return &controlador
+}
+
+func addTerremoto(t terremoto.Terremoto){
+
+}
