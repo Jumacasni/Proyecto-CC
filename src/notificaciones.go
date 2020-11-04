c403d7aa0155f4c6bbc93ea8d8e4c002e15f54f2
Juan Manuel Castillo Nievas <jumacasni@gmail.com>
2020-11-04 08:55:26 +0100

    creada clase para gestionar notificaciones. #71 #72 #73

---
+++ src/notificaciones.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"src/terremoto"
+)
+
+type Notificaciones struct {
+	// Emails registrados en el sistema
+	emails map[string]string
+}
+
+func NewNotificaciones() *Notificaciones{
+	// Inicializa los emails
+	notificaciones := Notificaciones{emails: make(map[string]string)
+	return &notificaciones
+}
