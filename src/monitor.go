b346c1d9f7558b1a2554095a2268c1784a38a549
Juan Manuel Castillo Nievas <jumacasni@gmail.com>
2020-11-04 08:47:19 +0100

    creada clase monitor, añadida funcion que consulta terremotos. #70

---
+++ src/monitor.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"src/terremoto"
+)
+
+type Monitor struct {
+	// URL a la que se va a hacer la petición
+	url string  
+}
+
+func NewMonitor() *Monitor{
+	// Inicializa la URL
+	monitor := Monitor{url: "https://www.ign.es/web/ign/portal/sis-catalogo-terremotos?p_p_id=IGNSISCatalogoTerremotos_WAR_IGNSISCatalogoTerremotosportlet&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_cacheability=cacheLevelPage&p_p_col_id=column-1&p_p_col_count=1&_IGNSISCatalogoTerremotos_WAR_IGNSISCatalogoTerremotosportlet_jspPage=%2Fjsp%2Fterremoto.jsp"}
+
+	return &monitor
+}
+
+// Consultar de terremotos, devuelve una lista de terremotos
+func consultarTerremoto(fechaInicio string, fechaFinal string, latitudMinima float, latitudMaxima float, longitudMinima float, longitudMaxima float, profundidadMinima int, profundidadMaxima int, magnitudMinima float, magnitudMaxima float) []terremoto.Terremoto{
+
+}
